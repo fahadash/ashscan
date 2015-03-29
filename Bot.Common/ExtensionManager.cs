044ee437cd835135b378cbbc78e0b1d6ae916b22
fahadash <me@fahadash.com>
2015-03-29 11:29:19 -0400

    Mono Addins, with fix required

---
+++ Bot.Common/ExtensionManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    using System.IO;
+    using System.Net.Mime;
+    using System.Reflection;
+
+    using Mono.Addins;
+
+    public class ExtensionManager
+    {
+        static ExtensionManager()
+        {
+            var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+
+            AddinManager.AddinLoadError += (s, e) =>
+            {
+
+            };
+            AddinManager.Initialize(".", ".", ".");
+            AddinManager.Registry.Update();
+        }
+
+        public static ICommandHandler GetCommandHandler(string command)
+        {
+            var commands = AddinManager.GetExtensionObjects<ICommandHandler>();
+
+            return commands.FirstOrDefault(c => c.Commands.Any(n => string.Compare(command, n, StringComparison.OrdinalIgnoreCase) == 0));
+        }
+
+        public static IIrcController GetController()
+        {
+            return AddinManager.GetExtensionObjects<IIrcController>(true).FirstOrDefault();
+        }
+    }
+}
---
+++ Bot.Common/ICommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    using Mono.Addins;
+
+    [TypeExtensionPoint]
+    public interface ICommandHandler
+    {
+        IEnumerable<string> Commands { get; }
+
+        void Handle(IUserInfo oper, IEnumerable<string> tokens);
+    }
+}
